2125c07190f11a967a10de3ead5cf0f849de4d33
eduoliver93 <82895@student.dorset-college.ie>
2026-09-17 14:52:39 +0100

    Delete teste1.py

--- teste1.py
+++
@@ -1,8 +0,0 @@
-greeting = "Hello, World!"
-my_name = "Juan Huetle"
-age = 30
-
-print ( greeting)
-print(f"{greeting} Dorset Calling")
-print (f"your name is"{my_name} and You are {age}) year old")
-
